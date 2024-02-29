0bd302947309a3eb7f98c955255b10144dc4c971
Dina Polousova <dinapolousova@gmail.com>
2024-02-29 13:40:51 +0300

    react init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import './_vars.scss';
+import './_reset.scss';
+import './_base.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React App</h1>
+    </div>
+  );
+}
+
+export default App;
